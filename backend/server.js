2b7e32a5c9df78027aacec8a11f902b5f4e3f596
stelioskon <steliosnteloss@gmail.com>
2020-11-17 23:01:03 +0200

    added product routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
-
 import connectDb from "./config/db.js"
-import products from "./data/products.js"
+import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config()
 connectDb()
@@ -10,20 +9,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.send("Api is running!")
 })
+
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode at port ${PORT}`)
 })
-
-app.get("/api/products", (req, res) => {
-  res.json(products)
-})
-
-app.get("/api/product/:id", (req, res) => {
-  const product = products.find((prod) => prod._id === req.params.id)
-  res.json(product)
-})
-
-// // app.listen(5000, console.log("server runs on 5000"))
