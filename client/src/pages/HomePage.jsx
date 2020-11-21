a9eccab2d8b33d4096913f83311c114e9daf476b
stelioskon <steliosnteloss@gmail.com>
2020-11-21 15:12:54 +0200

    finished with product actions

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,29 +1,38 @@
-import React, { Fragment, useState, useEffect } from "react"
+import React, { Fragment, useEffect } from "react"
 import { Row, Col } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+
+//  Actions
+import { listProducts } from "../actions/productActions"
+
+// Components
 import Product from "../components/content/Product.jsx"
-import axios from "axios"
 
 const HomePage = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products")
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <Fragment>
       <h1 style={{ textAlign: "center" }}>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h3> We got this error .. {error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </Fragment>
   )
 }
--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 
-const reducer = combineReducers({})
+// Reducers
+import { productListReducer } from "./reducers/productReducers"
+
+const reducer = combineReducers({ productList: productListReducer })
 const initialState = {}
 const middleware = [thunk]
 const store = createStore(
