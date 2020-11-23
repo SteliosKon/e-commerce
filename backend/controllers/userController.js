546223b152552d4a42b3b4090f67a048c3dbd027
stelioskon <steliosnteloss@gmail.com>
2020-11-23 23:02:06 +0200

    jwt added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler"
-//import User from "../models/user.js"
+import User from "../models/user.js"
+import generateToken from "../utils/tokenGenerator.js"
 
 // @desc Authenticate user & get token
 // @route POST /api/users/login
@@ -7,7 +8,23 @@ import asyncHandler from "express-async-handler"
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
-  res.send({ email, password })
+
+  //Search for user
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    // authenticate
+    res.json({
+      _id: user._uid,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error("Invalid email or password")
+  }
 })
 
 export { authUser }
